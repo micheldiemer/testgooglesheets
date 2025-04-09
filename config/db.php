ac282d3c32987d88cd7e923aa626aff2d9916a3e
Michel Diemer <michel.diemer@yahoo.fr>
2025-04-10 01:04:21 +0200

    bdd et framework

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'sqlite::memory:',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
